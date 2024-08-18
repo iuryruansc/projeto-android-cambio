69b46f0302000e2da8c6bd77251109cd14ac6b79
iuryruansc <iuryruansc@gmail.com>
2024-08-18 17:54:21 -0300

    adding catch IOException

--- app/src/main/java/com/betrybe/currencyview/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/currencyview/ui/views/activities/MainActivity.kt
@@ -18,7 +18,9 @@ import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.google.android.material.textview.MaterialTextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 
@@ -26,7 +28,7 @@ class MainActivity : AppCompatActivity() {
 
     private val mApiService = ApiServiceClient.instance
     private val mSelectionLayout: MaterialAutoCompleteTextView by lazy { findViewById(R.id.currency_selection_input_layout) }
-    private val mLoadCurrencyState: MaterialTextView by lazy {findViewById(R.id.load_currency_state)}
+    private val mLoadCurrencyState: MaterialTextView by lazy { findViewById(R.id.load_currency_state) }
     private val mSelectCurrencyState: MaterialTextView by lazy { findViewById(R.id.select_currency_state) }
     private val mWaitingResponseState: FrameLayout by lazy { findViewById(R.id.waiting_response_state) }
     private val mCurrencyRatesState: RecyclerView by lazy { findViewById(R.id.currency_rates_state) }
@@ -50,107 +52,145 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun fetchCurrenciesSymbols() {
+    private fun fetchCurrenciesSymbols(retryCount: Int = 3) {
         CoroutineScope(Dispatchers.IO).launch {
-            try {
-                ApiIdlingResource.increment()
+            repeat(retryCount) { attempt ->
+                try {
+                    ApiIdlingResource.increment()
 
-                val responseCurrencySymbol = mApiService.getSymbols()
+                    val responseCurrencySymbol = mApiService.getSymbols()
 
-                if (responseCurrencySymbol.isSuccessful) {
-                    val responseData = responseCurrencySymbol.body()!!
-                    currencyCodesLiveData.postValue(responseData.symbols.values.toList())
-                    val currencyCodes = responseData.symbols.keys.toList()
+                    if (responseCurrencySymbol.isSuccessful) {
+                        val responseData = responseCurrencySymbol.body()!!
+                        currencyCodesLiveData.postValue(responseData.symbols.values.toList())
+                        val currencyCodes = responseData.symbols.keys.toList()
 
-                    runOnUiThread {
-                        val adapter = CurrencyArrayAdapter(
-                            this@MainActivity,
-                            R.layout.currency_item,
-                            currencyCodes
-                        )
-                        mSelectionLayout.setAdapter(adapter)
+                        runOnUiThread {
+                            val adapter = CurrencyArrayAdapter(
+                                this@MainActivity,
+                                R.layout.currency_item,
+                                currencyCodes
+                            )
+                            mSelectionLayout.setAdapter(adapter)
 
-                        mLoadCurrencyState.visibility = View.GONE
-                        mSelectCurrencyState.visibility = View.VISIBLE
+                            mLoadCurrencyState.visibility = View.GONE
+                            mSelectCurrencyState.visibility = View.VISIBLE
+                        }
+                    } else {
+                        //Handle error
                     }
-                } else {
-                    //Handle error
-                }
 
-                ApiIdlingResource.decrement()
-            } catch (e: HttpException) {
-                //Handle error
-                ApiIdlingResource.decrement()
-            } catch (e: IOException) {
-                runOnUiThread {
-                    MaterialAlertDialogBuilder(this@MainActivity)
-                        .setTitle("Erro")
-                        .setMessage("Falha ao carregar moedas... ${e.message}")
-                        .setPositiveButton("Retry") {dialog, _ ->
-                            dialog.dismiss()
-                            fetchCurrenciesSymbols()
+                    ApiIdlingResource.decrement()
+                    return@repeat
+                } catch (e: HttpException) {
+                    ApiIdlingResource.decrement()
+                    //Handle error
+                } catch (e: IOException) {
+                    ApiIdlingResource.decrement()
+                    if (attempt < retryCount - 1) {
+                        val delayMillis = (pow(
+                            2, (attempt + 1)
+                                .coerceAtLeast(1)
+                        ) * 1000L).coerceAtMost(5000L)
+                        delay(delayMillis)
+                    } else {
+                        withContext(Dispatchers.Main) {
+                            showSymbolsErrorDialog(e.message)
                         }
-                        .setNegativeButton("Cancel") { dialog, _ ->
-                            dialog.dismiss()
-                            finish()
-                        }
-                        .show()
+                    }
                 }
-                ApiIdlingResource.decrement()
             }
         }
     }
 
-    private fun fetchCurrenciesRates() {
+    private fun fetchCurrenciesRates(retryCount: Int = 3) {
         CoroutineScope(Dispatchers.IO).launch {
-            try {
-                ApiIdlingResource.increment()
+            repeat(retryCount) { attempt ->
+                try {
+                    ApiIdlingResource.increment()
 
-                val symbol = mSelectionLayout.text.toString()
-                val responseCurrencyRate = mApiService.getLatestRates(symbol)
+                    val symbol = mSelectionLayout.text.toString()
+                    val responseCurrencyRate = mApiService.getLatestRates(symbol)
 
-                val codes = currencyCodesLiveData.value!!
+                    val codes = currencyCodesLiveData.value!!
 
-                if (responseCurrencyRate.isSuccessful) {
-                    val rates= responseCurrencyRate.body()?.rates
-                    runOnUiThread {
-                        if (!rates.isNullOrEmpty()) {
+                    if (responseCurrencyRate.isSuccessful) {
+                        val rates = responseCurrencyRate.body()?.rates
+                        runOnUiThread {
+                            if (!rates.isNullOrEmpty()) {
 
-                            val ratesAdapter = CurrencyRatesAdapter(rates, codes)
-                            mCurrencyRatesState.adapter = ratesAdapter
-                            ratesAdapter.notifyDataSetChanged()
+                                val ratesAdapter = CurrencyRatesAdapter(rates, codes)
+                                mCurrencyRatesState.adapter = ratesAdapter
+                                ratesAdapter.notifyDataSetChanged()
 
-                            mWaitingResponseState.visibility = View.GONE
-                            mCurrencyRatesState.visibility = View.VISIBLE
-                        } else {
-                            //Handle empty
+                                mWaitingResponseState.visibility = View.GONE
+                                mCurrencyRatesState.visibility = View.VISIBLE
+                                return@runOnUiThread
+                            } else {
+                                //Handle empty
+                            }
+                        }
+                    } else {
+                        //Handle error
+                    }
+                    ApiIdlingResource.decrement()
+                } catch (e: HttpException) {
+                    ApiIdlingResource.decrement()
+                    //Handle error
+                } catch (e: IOException) {
+                    ApiIdlingResource.decrement()
+                    if (attempt < retryCount - 1) {
+                        val delayMillis = (pow(
+                            2, (attempt + 1)
+                                .coerceAtLeast(1)
+                        ) * 1000L).coerceAtMost(5000L)
+                        delay(delayMillis)
+                    } else {
+                        withContext(Dispatchers.Main) {
+                            showCurrenciesErrorDialog(e.message)
                         }
                     }
-                } else {
-                    //Handle error
                 }
-
-                ApiIdlingResource.decrement()
-            } catch (e: HttpException) {
-                //Handle error
-                ApiIdlingResource.decrement()
-            } catch (e: IOException) {
-                runOnUiThread {
-                    MaterialAlertDialogBuilder(this@MainActivity)
-                        .setTitle("Erro")
-                        .setMessage("Falha ao carregar conversões da moeda escolhida... ${e.message}")
-                        .setPositiveButton("Retry") {dialog, _ ->
-                            dialog.dismiss()
-                            fetchCurrenciesSymbols()
-                        }
-                        .setNegativeButton("Cancel") { dialog, _ ->
-                            dialog.dismiss()
-                            finish()
-                        }
-                        .show()
-                }
-                ApiIdlingResource.decrement()
             }
         }
+    }
+
+    private fun showCurrenciesErrorDialog(errorMessage: String?) {
+        MaterialAlertDialogBuilder(this@MainActivity)
+            .setTitle("Erro")
+            .setMessage("Falha ao carregar conversões da moeda escolhida... $errorMessage")
+            .setPositiveButton("Retry") { dialog, _ ->
+                dialog.dismiss()
+                fetchCurrenciesRates()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+            .show()
+    }
+
+    private fun showSymbolsErrorDialog(errorMessage: String?) {
+        MaterialAlertDialogBuilder(this@MainActivity)
+            .setTitle("Error")
+            .setMessage("Failed to load currencies... $errorMessage")
+            .setPositiveButton("Retry") { dialog, _ ->
+                dialog.dismiss()
+                fetchCurrenciesSymbols()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+                finish()
+            }
+            .show()
+    }
+
+    private fun pow(base: Long, exponent: Int): Long {
+        if (exponent == 0) return 1
+        var result = base
+        for (i in 2..exponent) {
+            result *= base
+        }
+        return result
     }
 }
